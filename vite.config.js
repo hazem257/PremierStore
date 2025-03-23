18b4fcd0cf2144fc1dc4dd33de2f12d530d0519e
‪Hazem Gmall‬‏ <161766768+hazem257@users.noreply.github.com>
2025-03-23 22:37:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ const viteConfig = {
+  eslint : {
+    ignoreDuringBuildes : true ,
+  }
+ }
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+module.exports = viteConfig
